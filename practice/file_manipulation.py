d51cc5f27c10f2cf372353f344d09fec38cd90de
apolskiy <apolskiy@yahoo.com>
2025-11-03 18:04:44 -0800

    modified file_manipulation.py to produce json and excel files from csv, then open them and print their contents as a table

--- practice/file_manipulation.py
+++ practice/file_manipulation.py
@@ -1,35 +1,50 @@
 import os
+import sys
 import pandas as pd
 import csv
-file_path = 'my_data.txt'
+file_path = 'flowers.csv'
 data = []
-if os
-with open(file_path, 'r') as file:
-    for line in file:
-        data.append(line.strip()) # .strip() removes leading/trailing whitespace including newlines
+
+if os.path.exists(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            data.append(line.strip()) # .strip() removes leading/trailing whitespace including newlines
+else:
+    print(f"Error: File '{file_path}' not found.")
+    sys.exit(1)
 print(data)
 
 
-file_path = 'my_data.csv'
+file_path = 'flowers.csv'
 data = []
 with open(file_path, 'r', newline='') as file:
     reader = csv.reader(file)
     for row in reader:
         data.append(row)
+file.close()
 print(data)
 
 
 
-file_path = 'my_data.csv'
+file_path = 'flowers.csv'
 df = pd.read_csv(file_path)
+print("Printing Data Frame")
 print(df.head())
 
-file_path = 'my_data.xlsx'
+
+df.to_excel('flowers.xlsx', index=False)
+df.to_json('flowers.json', index=False)
+
+
+
+file_path = 'flowers.xlsx'
 df = pd.read_excel(file_path)
+print("Printing Data Frame from Excel")
 print(df.head())
 
 
-file_path = 'my_data.json'
+file_path = 'flowers.json'
 df = pd.read_json(file_path)
+print("Printing Data Frame from JSON")
 print(df.head())
 
