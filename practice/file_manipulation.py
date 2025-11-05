1eec48f00650301aa8c75aea62b7cea65fd163fa
apolskiy <apolskiy@yahoo.com>
2025-11-05 13:15:32 -0800

    Improved comments and logging in file_manipulation.py and PythonSQLFD.py

--- practice/file_manipulation.py
+++ practice/file_manipulation.py
@@ -1,5 +1,5 @@
 #Aleksandr Polskiy practice of file manipulation
-#taking .csv file as an imput line by line then printing it
+#taking .csv file as an input line by line then printing it
 #reading the file into dataframe printing it
 #saving it as .xlsx and .json
 #opening them reading them into dataframe and printing those dataframes
@@ -8,6 +8,7 @@ import os
 import sys
 import pandas as pd
 import csv
+from tabulate import tabulate
 file_path = 'flowers.csv'
 data = []
 
@@ -15,42 +16,38 @@ if os.path.exists(file_path):
     with open(file_path, 'r') as file:
         for line in file:
             data.append(line.strip()) # .strip() removes leading/trailing whitespace including newlines
+    file.close()
 else:
     print(f"Error: File '{file_path}' not found.")
     sys.exit(1)
-print(data)
-
-
-file_path = 'flowers.csv'
-data = []
-with open(file_path, 'r', newline='') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        data.append(row)
-file.close()
+print("Printing Data from CSV file as array")
 print(data)
 
 
 
 file_path = 'flowers.csv'
 df = pd.read_csv(file_path)
-print("Printing Data Frame")
+print("\nPrinting unformatted extracted Data Frame from csv file:")
 print(df.head())
 
-
+#writing dataframe contents into excel and json formatted files
 df.to_excel('flowers.xlsx', index=False)
 df.to_json('flowers.json', index=False)
 
 
-
+#import previously created excel file into another dataframe and print the dataframe
 file_path = 'flowers.xlsx'
 df1 = pd.read_excel(file_path)
-print("Printing Data Frame from Excel")
+print("\nPrinting Unformatted Data Frame from Excel:")
 print(df1.head())
 
+print("\nPrinting Tabulated Data Frame from Excel:")
+print(tabulate(df1, showindex=False, headers=df1.columns, numalign="left"))
 
+#open previously created json file, extract data into a dataframe and print the dataframe
 file_path = 'flowers.json'
 df2 = pd.read_json(file_path)
-print("Printing Data Frame from JSON")
-print(df2.head())
+print("\nPrinting tabulated with header separation DataFrame from JSON:")
+#print(df2.head())
+print(tabulate(df2, showindex=False, headers=df2.columns, numalign="left",tablefmt="pretty"))
 
