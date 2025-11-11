7b40990a33271c1cae509bb11b0c25c9499109f9
apolskiy <apolskiy@yahoo.com>
2025-11-11 08:31:54 -0800

    syslog_practice_message_extraction.py adjusted for PYLINT

--- practice/syslog_practice_message_extraction.py
+++ practice/syslog_practice_message_extraction.py
@@ -2,6 +2,8 @@
 #Aleksandr Polskiy practice of extracting messages by application from syslog.log
 #calculating user statistic for INFO and ERROR messages for the app
 #Determining the error message counts per user
+"""This script extracts messages by application from syslog.log
+without using any OS command line tools"""
 
 import re
 import operator
@@ -13,39 +15,39 @@ per_user = {}  # Splitting between INFO and ERROR
 errors = {}
 
 # * Read file and create dictionaries
-with open('syslog.log') as file:
+with open('syslog.log',encoding='utf-8') as file:
     # read each line
     for line in file.readlines():
         # regex search
         # * Sample Line of log file
         # "May 27 11:45:40 ubuntu.local ticky: INFO: Created ticket [#1234] (username)"
         match = re.search(r"ticky: ([\w+]*):? ([\w' ]*)[\[[#0-9]*\]?]? ?\((.*)\)$", line)
-        if match != None:
-           code, error_msg, user = match.group(1), match.group(2), match.group(3)
+        if match is not None:
+            code, error_msg, user = match.group(1), match.group(2), match.group(3)
 
-        # Populates error dict with ERROR messages from log file
-        if error_msg not in errors.keys():
-            errors[error_msg] = 1
-        else:
-            errors[error_msg] += 1
-        # Populates per_user dict with users and default values
-        if user not in per_user.keys():
-            per_user[user] = {}
-            per_user[user]['INFO'] = 0
-            per_user[user]['ERROR'] = 0
-        # Populates per_user dict with users logs entry
-        if code == 'INFO':
+            # Populates error dict with ERROR messages from log file
+            if error_msg not in errors.keys():
+                errors[error_msg] = 1
+            else:
+                errors[error_msg] += 1
+            # Populates per_user dict with users and default values
             if user not in per_user.keys():
                 per_user[user] = {}
                 per_user[user]['INFO'] = 0
-            else:
-                per_user[user]["INFO"] += 1
-        elif code == 'ERROR':
-            if user not in per_user.keys():
-                per_user[user] = {}
-                per_user[user]['INFO'] = 0
-            else:
-                per_user[user]['ERROR'] += 1
+                per_user[user]['ERROR'] = 0
+            # Populates per_user dict with users logs entry
+            if code == 'INFO':
+                if user not in per_user.keys():
+                    per_user[user] = {}
+                    per_user[user]['INFO'] = 0
+                else:
+                    per_user[user]["INFO"] += 1
+            elif code == 'ERROR':
+                if user not in per_user.keys():
+                    per_user[user] = {}
+                    per_user[user]['INFO'] = 0
+                else:
+                    per_user[user]['ERROR'] += 1
 
 
 # Sorted by VALUE (Most common to least common)
@@ -59,13 +61,13 @@ file.close()
 errors_list.insert(0, ('Error', 'Count'))
 
 # * Create CSV file user_statistics
-with open('user_statistics_practice.csv', 'w', newline='') as user_csv:
+with open('user_statistics_practice.csv', 'w', newline='',encoding='utf-8') as user_csv:
     user_csv.write('Username,INFO,ERROR\n')
     for key, value in per_user_list:
         user_csv.write(str(key) + ',' +
                        str(value['INFO']) + ',' + str(value['ERROR'])+'\n')
 
 # * Create CSV error_message
-with open('error_message_practice.csv', 'w', newline='') as error_csv:
+with open('error_message_practice.csv', 'w', newline='',encoding='utf-8') as error_csv:
     for key, value in errors_list:
         error_csv.write(str(key) + ' ' + str(value)+'\n')
